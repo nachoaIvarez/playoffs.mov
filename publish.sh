ddfa31bb901fc0a6ca8af80dabe33cad103e6eba
Nacho Álvarez <pgp@nachoalvarez.dev>
2024-08-24 03:42:28 -0300

    feat: setup project specific builder

--- publish.sh
+++ publish.sh
@@ -8,11 +8,27 @@ fi
 
 VERSION=$1
 
+# Store the name of the current builder
+ORIGINAL_BUILDER=$(docker buildx use | awk '{print $NF}')
+
 # Update the LABEL version in the Dockerfile
 sed -i.bak -E "s/(LABEL version=\")[^\"]+(\")/\1${VERSION}\2/" Dockerfile
 
+# Check if the builder exists, create it if it doesn't
+if ! docker buildx inspect playoffs-mov-builder > /dev/null 2>&1; then
+  echo "Creating new builder: playoffs-mov-builder"
+  docker buildx create --name playoffs-mov-builder
+fi
+
+# Use the playoffs-mov-builder
+docker buildx use playoffs-mov-builder
+
 # Build and push the multi-platform docker image
-docker build -t ghcr.io/nachoaivarez/playoffs.mov:${VERSION} -t ghcr.io/nachoaivarez/playoffs.mov:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64 \
+  -t ghcr.io/nachoaivarez/playoffs.mov:${VERSION} \
+  -t ghcr.io/nachoaivarez/playoffs.mov:latest \
+  --push .
+
 if [ $? -ne 0 ]; then
   echo "Docker build failed"
   exit 1
@@ -24,4 +40,8 @@ git commit -m "${VERSION}"
 git tag "${VERSION}"
 git push origin master --tags
 
-echo "Docker image successfully built, tagged, and pushed."
+echo "Multi-architecture Docker image successfully built, tagged, and pushed."
+
+# Switch back to the original builder
+docker buildx use "$ORIGINAL_BUILDER"
+echo "Switched back to the original builder: $ORIGINAL_BUILDER"
